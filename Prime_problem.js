72bcfd34bd9c7c822f284b1ce336cbbda5cb4edd
Neelachari N H <neelachari34@gmail.com>
2022-09-22 16:42:47 +0530

    Please check the code

---
+++ Prime_problem.js
@@ -0,0 +1,14 @@
+let number = 7;
+let count = 0;
+
+for (let i = 0; i <= number; i++) {
+  if (number % i == 0) {
+    count++;
+  }
+}
+if (count >= 2) {
+  console.log(number, "is a prime");
+}
+else {
+  console.log(number, "is Not a prime");
+}
